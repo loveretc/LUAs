4d35e6f60a810fd443b113ae8216818d2ec6c55c
revol <49048914+loveretc@users.noreply.github.com>
2019-09-12 12:16:30 -0300

    Add files via upload

---
+++ LUAs/mindmg.lua
@@ -0,0 +1,54 @@
+-------- Local --------
+local ui_new_slider = ui.new_slider
+local ui_new_hotkey = ui.new_hotkey
+local ui_set = ui.set
+local ui_get = ui.get
+
+local c_draw = client.draw_text
+local c_size = client.screen_size
+
+local reduce = false
+
+-------- Fix for auto and HP+i --------
+
+local damagefix = {
+    [0] = "Auto",
+}
+
+for i=1, 26 do
+    damagefix[100 + i] = "HP + " .. i
+end
+
+-------- Menu draw --------
+local hotkey = ui.new_checkbox('LUA', 'B', 'Minimum damage override')
+local hotkey = ui.new_hotkey('LUA', 'B', 'Minimum damage override', true)
+local slider_dmg = ui_new_slider('LUA', 'B', 'Ammount', 0, 126, 0, true, "", 1, damagefix)
+
+local min_dmg = ui.reference('RAGE', 'aimbot', 'minimum damage')
+
+-------- Actual override --------
+
+local function reduction(c)
+local h_key = ui.get(hotkey)
+local dmg_slider_ref = ui_get(slider_dmg)
+
+local sw, sh = c_size()
+local x = sw / 2
+local y = sh - 200
+	
+	if h_key and reduce == false then
+		stored_dmg = ui_get(min_dmg)
+		ui_set(min_dmg, dmg_slider_ref)
+		reduce = true
+	elseif not h_key and reduce == true then
+		ui_set(min_dmg, stored_dmg)
+		reduce = false
+	end	
+		
+	if reduce then
+		c_draw(c, 58, y + 85, 124, 195, 13, 255, "c+", 0, "DAMAGE")
+	end		
+end
+
+
+client.set_event_callback("paint", reduction)
