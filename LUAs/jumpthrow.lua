fe050d4e57413532eb5d209d3c2496a4d63883c6
revol <49048914+loveretc@users.noreply.github.com>
2019-09-12 12:17:22 -0300

    Update jumpthrow.lua

--- LUAs/jumpthrow.lua
+++ LUAs/jumpthrow.lua
@@ -1,9 +1,9 @@
--- LOCAL 
+-------- Local -------- 
 local throw = ui.new_checkbox ("MISC", "Settings", "Jumpthrow")
 local throw = ui.new_hotkey("MISC", "Settings", "Jumpthrow", true)
 
 
--- Code
+-------- Jumpthrow --------
 client.set_event_callback("setup_command", function(c)
     if ui.get(throw) then
         local weapon = entity.get_player_weapon(entity.get_local_player())
