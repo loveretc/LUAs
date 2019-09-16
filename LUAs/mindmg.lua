2340984ad826c5190c6ac49cf65e14eced4394e4
revol <49048914+loveretc@users.noreply.github.com>
2019-09-16 15:12:38 -0300

    Now draws an actual indicator

--- LUAs/mindmg.lua
+++ LUAs/mindmg.lua
@@ -4,7 +4,7 @@ local ui_new_hotkey = ui.new_hotkey
 local ui_set = ui.set
 local ui_get = ui.get
 
-local c_draw = client.draw_text
+local c_draw = client.draw_indicator
 local c_size = client.screen_size
 
 local reduce = false
@@ -21,15 +21,14 @@ end
 
 -------- Menu draw --------
 local hotkey = ui.new_checkbox('LUA', 'B', 'Minimum damage override')
-local hotkey = ui.new_hotkey('LUA', 'B', 'Minimum damage override', true)
+local hotkey_hk = ui.new_hotkey('LUA', 'B', 'Minimum damage override hotkey', true)
 local slider_dmg = ui_new_slider('LUA', 'B', 'Ammount', 0, 126, 0, true, "", 1, damagefix)
 
 local min_dmg = ui.reference('RAGE', 'aimbot', 'minimum damage')
 
 -------- Actual override --------
-
 local function reduction(c)
-local h_key = ui.get(hotkey)
+local h_key = ui.get(hotkey_hk)
 local dmg_slider_ref = ui_get(slider_dmg)
 
 local sw, sh = c_size()
@@ -44,11 +43,13 @@ local y = sh - 200
 		ui_set(min_dmg, stored_dmg)
 		reduce = false
 	end	
-		
+
+-------- Actual indicator --------
+local overriding = ui.get(hotkey)
+
 	if reduce then
-		c_draw(c, 58, y + 85, 124, 195, 13, 255, "c+", 0, "DAMAGE")
-	end		
+		c_draw(c, 124, 195, 21, 255, "DAMAGE")
+	end	
+
 end
-
-
 client.set_event_callback("paint", reduction)
